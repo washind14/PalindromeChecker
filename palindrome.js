176809fd3ca21c1bd95fd576c0e8f33a3ac4aeaf
Desha Washington <washind14@icloud.com>
2026-05-31 20:17:55 -0500

    updated falsy check

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,16 @@
 const input = document.getElementById("input")
 
+function reverseString(str) {
+
+}
+
 function check() {
   const value = input.value;
-  // alert(value)
 
-  if (value.split('').reverse().join("") === value) {
-    alert("This is a palindrome");
+  if (!value) {
+    alert("Please enter text!");
+  } else if (value.split('').reverse().join("") === value){
+    alert("This is a palindrome")
   } else {
     alert("Not a palindrome!")
   }
